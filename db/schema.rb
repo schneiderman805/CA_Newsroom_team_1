11a31dc245f1cdc415390ccfb9a46bc727b8f617
William Schneiderman <william.m.schneiderman@gmail.com>
2019-01-17 16:02:08 +0100

    changes 6 to 3 to compinsate for validation change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_16_070139) do
+ActiveRecord::Schema.define(version: 2019_01_16_152908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,17 +56,12 @@ ActiveRecord::Schema.define(version: 2019_01_16_070139) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.text "content"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-<<<<<<< HEAD
-    t.bigint "user_id"
-=======
-    t.bigint "article_id"
     t.string "commenter"
     t.text "body"
->>>>>>> 5acf2059a5d4881b56bac1b3892d7290ae37b8d8
+    t.bigint "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
