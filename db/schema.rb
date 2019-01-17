2e58cfdab2b04b2913dccd490b03fbbf91383ee9
Aditya Naik <naik.aditya@gmail.com>
2019-01-17 19:54:27 +0100

    Updates schema

--- db/schema.rb
+++ db/schema.rb
@@ -63,14 +63,6 @@ ActiveRecord::Schema.define(version: 2019_01_17_110411) do
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
-  
-  create_table "transactions", force: :cascade do |t|
-    t.integer "amount"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.index ["user_id"], name: "index_transactions_on_user_id"
-  end
 
   create_table "transactions", force: :cascade do |t|
     t.integer "amount"
