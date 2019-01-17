3c885f1fd66e3d2d473616d5203d3aadb2628bf4
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-17 15:55:22 +0100

    Removes unneccesary new action form transaction controller

--- app/controllers/cms/transactions_controller.rb
+++ app/controllers/cms/transactions_controller.rb
@@ -1,8 +1,4 @@
 class Cms::TransactionsController < ApplicationController
-    
-    def new
-        @user = User.find_by_id(current_user.id)
-    end
 
     def create
         @user = User.find_by_id(current_user.id)
