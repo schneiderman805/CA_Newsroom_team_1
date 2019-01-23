5eb8101c2f7e63f53e37dfc9c4fb3fd8af842ec5
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-23 15:50:33 +0100

    Fixes indentation

--- app/serializers/articles/index_serializer.rb
+++ app/serializers/articles/index_serializer.rb
@@ -1,14 +1,14 @@
 class Articles::IndexSerializer < ActiveModel::Serializer
-	include Rails.application.routes.url_helpers
-	attributes :id, :title, :lede, :body, :image
-	belongs_to :user, serializer: Users::UserInfoSerializer
-	belongs_to :category, serializer: Categories::CategoriesInfoSerializer
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :lede, :body, :image
+  belongs_to :user, serializer: Users::UserInfoSerializer
+  belongs_to :category, serializer: Categories::CategoriesInfoSerializer
 
-	def image
-		if Rails.env.test?
-			rails_blob_url(object.image)
-		else
-			object & image&.service_url(expires_in: 1.minute, disposition: 'inline')
-		end
-	end
+  def image
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object & image&.service_url(expires_in: 1.minute, disposition: 'inline')
+    end
+  end
 end
