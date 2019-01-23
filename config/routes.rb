ef674898788c803d827af24d6b24922831e12791
Gregory Kallai <gerg.kallai@gmail.com>
2019-01-23 07:51:09 +0100

    Refactors update role method from articles controller into users controller, aligns quotes in files

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   namespace :cms do
     resources :articles
     resources :transactions, only: [:new, :create]
+    resources :users, only: [:update]
   end
 
 end
